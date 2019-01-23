ee7846d625da9dd19b2a3e2e3b22a8f3167a0215
lDark-Moonl <eunomya@gmail.com>
2019-01-23 17:32:03 +0100

    Video player and uploader plus the score list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,17 @@ export class AppComponent {
   player: ElementRef;
 
 
+  private _items = [1,2,3,4,5,6,7,8];
+
   public onchange(event){
     let test = URL.createObjectURL(event.target.files[0]);
     this.player.nativeElement.src = test;
   }
 
+
+  get items() {
+    return this._items.sort()
+  }
 
 
 }
